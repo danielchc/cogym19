3287517f1fb15130313c36bb5e7d37c582f1a6bc
David <davidcm62@gmail.com>
2020-03-25 12:38:51 +0100

    Commit

---
+++ CentroDeportivo/src/aplicacion/Usuario.java
@@ -0,0 +1,12 @@
+package aplicacion;
+
+public class Usuario {
+    private String login;
+    private String contrasinal;
+    private String nome;
+    private String numTelefono;
+    private String DNI;
+    private String correoElectronico;
+    private String IBANconta;
+    
+}
