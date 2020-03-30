77d2df13a2dc6205f200d158ffa2d31c70129973
David <davidcm62@gmail.com>
2020-03-30 17:17:35 +0200

    Commit

---
+++ CentroDeportivo/src/centrodeportivo/gui/controladores/persoal/vNovoPersoalController.java
@@ -0,0 +1,11 @@
+package centrodeportivo.gui.controladores.persoal;
+
+import javafx.event.ActionEvent;
+
+public class vNovoPersoalController {
+
+
+
+    public void btnGardarAccion(ActionEvent actionEvent) {
+    }
+}
