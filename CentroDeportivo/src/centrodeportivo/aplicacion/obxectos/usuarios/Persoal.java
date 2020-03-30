c8a4b49537f24c1e71ac8a6fee871ead7925aba9
David <davidcm62@gmail.com>
2020-03-30 10:24:41 +0200

    Commit

--- CentroDeportivo/src/centrodeportivo/aplicacion/obxectos/usuarios/Persoal.java
+++ CentroDeportivo/src/centrodeportivo/aplicacion/obxectos/usuarios/Persoal.java
@@ -6,7 +6,6 @@ import centrodeportivo.aplicacion.obxectos.usuarios.Usuario;
 import java.sql.Date;
 
 public class Persoal extends Usuario {
-    private Date dataIncorporacion;
     private String NUSS;
 
     public Persoal(String login){
@@ -14,9 +13,8 @@ public class Persoal extends Usuario {
         super.setTipoUsuario(TipoUsuario.Persoal);
     }
 
-    public Persoal(String login,String contrasinal,String nome,String numTelefono,String DNI,String correoElectronico,String IBANconta,Date dataAlta,Date dataIncorporacion,String NUSS){
+    public Persoal(String login,String contrasinal,String nome,String numTelefono,String DNI,String correoElectronico,String IBANconta,Date dataAlta,String NUSS){
         super(login, contrasinal, nome, numTelefono, DNI, correoElectronico, IBANconta, dataAlta);
-        this.dataIncorporacion=dataIncorporacion;
         this.NUSS=NUSS;
     }
 
@@ -25,13 +23,6 @@ public class Persoal extends Usuario {
         this.NUSS=NUSS;
     }
 
-    public Date getDataIncorporacion() {
-        return dataIncorporacion;
-    }
-
-    public void setDataIncorporacion(Date dataIncorporacion) {
-        this.dataIncorporacion = dataIncorporacion;
-    }
 
     public String getNUSS() {
         return NUSS;
--- CentroDeportivo/src/centrodeportivo/aplicacion/obxectos/usuarios/Profesor.java
+++ CentroDeportivo/src/centrodeportivo/aplicacion/obxectos/usuarios/Profesor.java
@@ -11,7 +11,7 @@ public final class Profesor extends Persoal {
         super.setTipoUsuario(TipoUsuario.Profesor);
     }
     public Profesor(String login,String contrasinal,String nome,String numTelefono,String DNI,String correoElectronico,String IBANconta,Date dataAlta,Date dataIncorporacion,String NUSS){
-        super(login, contrasinal, nome, numTelefono, DNI, correoElectronico, IBANconta, dataAlta, dataIncorporacion, NUSS);
+        super(login, contrasinal, nome, numTelefono, DNI, correoElectronico, IBANconta, dataAlta, NUSS);
     }
     public Profesor(String login,String contrasinal,String nome,String numTelefono,String DNI,String correoElectronico,String IBANconta,String NUSS){
         super(login, contrasinal, nome, numTelefono, DNI, correoElectronico, IBANconta, NUSS);
