b72a843460bd66373d22b2f4fbd59ffcf7395eea
Dani <danielchenelcea@gmail.com>
2020-03-31 15:17:38 +0200

    Commit

--- CentroDeportivo/src/centrodeportivo/baseDatos/DAOIncidencias.java
+++ CentroDeportivo/src/centrodeportivo/baseDatos/DAOIncidencias.java
@@ -17,93 +17,123 @@ public final class DAOIncidencias extends AbstractDAO {
         super(conexion,fachadaAplicacion);
     }
 
-    protected void insertarIncidencia(Incidencia incidencia) throws SQLException {
+    protected void insertarIncidencia(Incidencia incidencia) {
         PreparedStatement stmIncidencia=null;
-        if(incidencia.getTipoIncidencia()== TipoIncidencia.Area){
-            stmIncidencia=super.getConexion().prepareStatement("INSERT INTO incidenciasAreas(usuario,descricion,area,instalacion)  VALUES (?,?,?,?);");
-            stmIncidencia.setInt(3,incidencia.getArea().getCodArea());
-            stmIncidencia.setInt(4,incidencia.getArea().getInstalacion().getCodInstalacion());
-        }else{
-            stmIncidencia=super.getConexion().prepareStatement("INSERT INTO incidenciasMateriais(usuario,descricion,material)  VALUES (?,?,?);");
-            stmIncidencia.setInt(3,incidencia.getMaterial().getCodMaterial());
+        try {
+            if (incidencia.getTipoIncidencia() == TipoIncidencia.Area) {
+                stmIncidencia = super.getConexion().prepareStatement("INSERT INTO incidenciasAreas(usuario,descricion,area,instalacion)  VALUES (?,?,?,?);");
+                stmIncidencia.setInt(3, incidencia.getArea().getCodArea());
+                stmIncidencia.setInt(4, incidencia.getArea().getInstalacion().getCodInstalacion());
+            } else {
+                stmIncidencia = super.getConexion().prepareStatement("INSERT INTO incidenciasMateriais(usuario,descricion,material)  VALUES (?,?,?);");
+                stmIncidencia.setInt(3, incidencia.getMaterial().getCodMaterial());
+            }
+            stmIncidencia.setString(1, incidencia.getUsuario().getLogin());
+            stmIncidencia.setString(2, incidencia.getDescricion());
+            System.out.println(stmIncidencia);
+            stmIncidencia.executeUpdate();
+            super.getConexion().commit();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                stmIncidencia.close();
+            } catch (SQLException e) {
+                System.out.println("Imposible pechar os cursores.");
+            }
         }
-        stmIncidencia.setString(1,incidencia.getUsuario().getLogin());
-        stmIncidencia.setString(2,incidencia.getDescricion());
-        System.out.println(stmIncidencia);
-        stmIncidencia.executeUpdate();
-        super.getConexion().commit();
     }
 
-    protected ArrayList<Incidencia> listarIncidencia() throws SQLException {
+    protected ArrayList<Incidencia> listarIncidencia() {
         PreparedStatement stmIncidencia = null;
         ArrayList<Incidencia> incidencias=new ArrayList<Incidencia>();
         ResultSet rsIncidencias;
-        stmIncidencia = super.getConexion().prepareStatement("SELECT *,ar.nome AS nombreArea,it.nome AS nombreInstalacion,ia.descricion AS descricionIncidencia,ar.descricion AS descricionArea FROM incidenciasAreas AS ia JOIN areas AS ar ON ia.area=ar.codArea JOIN instalacions AS it  ON ar.instalacion=it.codInstalacion");
-        rsIncidencias = stmIncidencia.executeQuery();
-        Area area;
-        Instalacion instalacion;
-        while(rsIncidencias.next()){
-            instalacion=new Instalacion(
-                    rsIncidencias.getInt("codInstalacion"),
-                    rsIncidencias.getString("nombreInstalacion"),
-                    rsIncidencias.getString("numTelefono"),
-                    rsIncidencias.getString("direccion")
-            );
-            area=new Area(
-                    rsIncidencias.getInt("codArea"),
-                    instalacion,
-                    rsIncidencias.getString("nombreArea"),
-                    rsIncidencias.getString("descricionArea"),
-                    rsIncidencias.getInt("aforoMaximo"),
-                    rsIncidencias.getDate("dataBaixa")
-            );
-            incidencias.add(new Incidencia(
-                    TipoIncidencia.Area,
-                    rsIncidencias.getInt("numero"),
-                    new Usuario(rsIncidencias.getString("usuario")),
-                    rsIncidencias.getString("descricionIncidencia"),
-                    area
-            ));
-        }
-        stmIncidencia = super.getConexion().prepareStatement("SELECT *,im.descricion AS descricionIncidencia FROM incidenciasmateriais AS im JOIN materiais AS ma ON im.material=ma.codMaterial;");
-        rsIncidencias = stmIncidencia.executeQuery();
-        Material material;
-        while(rsIncidencias.next()){
-            material=new Material(
-                    rsIncidencias.getInt("numero"),
-                    new Area(rsIncidencias.getInt("area"),new Instalacion(rsIncidencias.getInt("instalacion"))),
-                    rsIncidencias.getString("nome"),
-                    rsIncidencias.getDate("dataCompra"),
-                    rsIncidencias.getDouble("prezoCompra")
-            );
-            incidencias.add(new Incidencia(
-                    TipoIncidencia.Material,
-                    rsIncidencias.getInt("numero"),
-                    new Usuario(rsIncidencias.getString("usuario")),
-                    rsIncidencias.getString("descricionIncidencia"),
-                    material
-            ));
+        try {
+            stmIncidencia = super.getConexion().prepareStatement("SELECT *,ar.nome AS nombreArea,it.nome AS nombreInstalacion,ia.descricion AS descricionIncidencia,ar.descricion AS descricionArea FROM incidenciasAreas AS ia JOIN areas AS ar ON ia.area=ar.codArea JOIN instalacions AS it  ON ar.instalacion=it.codInstalacion");
+            rsIncidencias = stmIncidencia.executeQuery();
+            Area area;
+            Instalacion instalacion;
+            while (rsIncidencias.next()) {
+                instalacion = new Instalacion(
+                        rsIncidencias.getInt("codInstalacion"),
+                        rsIncidencias.getString("nombreInstalacion"),
+                        rsIncidencias.getString("numTelefono"),
+                        rsIncidencias.getString("direccion")
+                );
+                area = new Area(
+                        rsIncidencias.getInt("codArea"),
+                        instalacion,
+                        rsIncidencias.getString("nombreArea"),
+                        rsIncidencias.getString("descricionArea"),
+                        rsIncidencias.getInt("aforoMaximo"),
+                        rsIncidencias.getDate("dataBaixa")
+                );
+                incidencias.add(new Incidencia(
+                        TipoIncidencia.Area,
+                        rsIncidencias.getInt("numero"),
+                        new Usuario(rsIncidencias.getString("usuario")),
+                        rsIncidencias.getString("descricionIncidencia"),
+                        area
+                ));
+            }
+            stmIncidencia = super.getConexion().prepareStatement("SELECT *,im.descricion AS descricionIncidencia FROM incidenciasmateriais AS im JOIN materiais AS ma ON im.material=ma.codMaterial;");
+            rsIncidencias = stmIncidencia.executeQuery();
+            Material material;
+            while (rsIncidencias.next()) {
+                material = new Material(
+                        rsIncidencias.getInt("numero"),
+                        new Area(rsIncidencias.getInt("area"), new Instalacion(rsIncidencias.getInt("instalacion"))),
+                        rsIncidencias.getString("nome"),
+                        rsIncidencias.getDate("dataCompra"),
+                        rsIncidencias.getDouble("prezoCompra")
+                );
+                incidencias.add(new Incidencia(
+                        TipoIncidencia.Material,
+                        rsIncidencias.getInt("numero"),
+                        new Usuario(rsIncidencias.getString("usuario")),
+                        rsIncidencias.getString("descricionIncidencia"),
+                        material
+                ));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                stmIncidencia.close();
+            } catch (SQLException e) {
+                System.out.println("Imposible pechar os cursores.");
+            }
         }
         return incidencias;
     }
 
-    protected void resolverIncidencia(Incidencia incidencia) throws SQLException {
+    protected void resolverIncidencia(Incidencia incidencia) {
         PreparedStatement stmIncidencia=null;
-        if(incidencia.getTipoIncidencia()==TipoIncidencia.Area) {
-            stmIncidencia = super.getConexion().prepareStatement("UPDATE incidenciasAreas SET comentarioResolucion=?, dataResolucion=NOW(), custoReparacion=?  WHERE numero=?;");
-            stmIncidencia.setString(1, incidencia.getComentarioResolucion());
-            stmIncidencia.setDouble(2, incidencia.getCustoReparacion());
-            stmIncidencia.setInt(3, incidencia.getNumero());
-            System.out.println(stmIncidencia);
-            stmIncidencia.executeUpdate();
-        }else {
-            stmIncidencia = super.getConexion().prepareStatement("UPDATE incidenciasMateriais SET comentarioResolucion=?, dataResolucion=NOW(), custoReparacion=?  WHERE numero=?;");
-            stmIncidencia.setString(1, incidencia.getComentarioResolucion());
-            stmIncidencia.setDouble(2, incidencia.getCustoReparacion());
-            stmIncidencia.setInt(3, incidencia.getNumero());
-            stmIncidencia.executeUpdate();
+        try {
+            if (incidencia.getTipoIncidencia() == TipoIncidencia.Area) {
+                stmIncidencia = super.getConexion().prepareStatement("UPDATE incidenciasAreas SET comentarioResolucion=?, dataResolucion=NOW(), custoReparacion=?  WHERE numero=?;");
+                stmIncidencia.setString(1, incidencia.getComentarioResolucion());
+                stmIncidencia.setDouble(2, incidencia.getCustoReparacion());
+                stmIncidencia.setInt(3, incidencia.getNumero());
+                System.out.println(stmIncidencia);
+                stmIncidencia.executeUpdate();
+            } else {
+                stmIncidencia = super.getConexion().prepareStatement("UPDATE incidenciasMateriais SET comentarioResolucion=?, dataResolucion=NOW(), custoReparacion=?  WHERE numero=?;");
+                stmIncidencia.setString(1, incidencia.getComentarioResolucion());
+                stmIncidencia.setDouble(2, incidencia.getCustoReparacion());
+                stmIncidencia.setInt(3, incidencia.getNumero());
+                stmIncidencia.executeUpdate();
+            }
+            super.getConexion().commit();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                stmIncidencia.close();
+            } catch (SQLException e) {
+                System.out.println("Imposible pechar os cursores.");
+            }
         }
-        super.getConexion().commit();
     }
 
 }
--- CentroDeportivo/src/centrodeportivo/baseDatos/DAOMensaxes.java
+++ CentroDeportivo/src/centrodeportivo/baseDatos/DAOMensaxes.java
@@ -16,60 +16,97 @@ public final class DAOMensaxes extends AbstractDAO {
         super(conexion,fachadaAplicacion);
     }
 
-    protected void enviarMensaxe(Mensaxe m) throws SQLException {
+    protected void enviarMensaxe(Mensaxe m) {
 
         PreparedStatement stmMensaxe=null;
-
-        stmMensaxe=super.getConexion().prepareStatement("INSERT INTO enviarMensaxes (emisor,receptor,dataEnvio,contido,lido) VALUES (?,?,NOW(),?,?);");
-        stmMensaxe.setString(1,m.getEmisor().getLogin());
-        stmMensaxe.setString(2,m.getReceptor().getLogin());
-        stmMensaxe.setString(3,m.getContido());
-        stmMensaxe.setBoolean(4,m.isLido());
-        stmMensaxe.executeUpdate();
-        super.getConexion().commit();
+        try {
+            stmMensaxe = super.getConexion().prepareStatement("INSERT INTO enviarMensaxes (emisor,receptor,dataEnvio,contido,lido) VALUES (?,?,NOW(),?,?);");
+            stmMensaxe.setString(1, m.getEmisor().getLogin());
+            stmMensaxe.setString(2, m.getReceptor().getLogin());
+            stmMensaxe.setString(3, m.getContido());
+            stmMensaxe.setBoolean(4, m.isLido());
+            stmMensaxe.executeUpdate();
+            super.getConexion().commit();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                stmMensaxe.close();
+            } catch (SQLException e) {
+                System.out.println("Imposible pechar os cursores.");
+            }
+        }
     }
 
-    protected void enviarMensaxe(Usuario emisor, ArrayList<Usuario> receptores,String mensaxe) throws SQLException{
+    protected void enviarMensaxe(Usuario emisor, ArrayList<Usuario> receptores,String mensaxe){
         PreparedStatement stmMensaxe=null;
-
-        for(Usuario receptor:receptores){
-            stmMensaxe=super.getConexion().prepareStatement("INSERT INTO enviarMensaxes (emisor,receptor,dataEnvio,contido,lido) VALUES (?,?,NOW(),?,?);");
-            stmMensaxe.setString(1,emisor.getLogin());
-            stmMensaxe.setString(2,receptor.getLogin());
-            stmMensaxe.setString(3,mensaxe);
-            stmMensaxe.setBoolean(4,false);
-            stmMensaxe.executeUpdate();
+        try{
+            for(Usuario receptor:receptores){
+                stmMensaxe=super.getConexion().prepareStatement("INSERT INTO enviarMensaxes (emisor,receptor,dataEnvio,contido,lido) VALUES (?,?,NOW(),?,?);");
+                stmMensaxe.setString(1,emisor.getLogin());
+                stmMensaxe.setString(2,receptor.getLogin());
+                stmMensaxe.setString(3,mensaxe);
+                stmMensaxe.setBoolean(4,false);
+                stmMensaxe.executeUpdate();
+            }
+            super.getConexion().commit();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                stmMensaxe.close();
+            } catch (SQLException e) {
+                System.out.println("Imposible pechar os cursores.");
+            }
         }
-        super.getConexion().commit();
     }
 
     protected void marcarMensaxeComoLido(Mensaxe m) throws SQLException{
         PreparedStatement stmMensaxe=null;
 
-        stmMensaxe=super.getConexion().prepareStatement("UPDATE enviarMensaxes SET lido=TRUE WHERE emisor=? AND receptor=? AND dataEnvio=?;");
-        stmMensaxe.setString(1,m.getEmisor().getLogin());
-        stmMensaxe.setString(2,m.getReceptor().getLogin());
-        stmMensaxe.setTimestamp(3,m.getDataEnvio());
-        stmMensaxe.executeUpdate();
-        super.getConexion().commit();
+        try{
+            stmMensaxe=super.getConexion().prepareStatement("UPDATE enviarMensaxes SET lido=TRUE WHERE emisor=? AND receptor=? AND dataEnvio=?;");
+            stmMensaxe.setString(1,m.getEmisor().getLogin());
+            stmMensaxe.setString(2,m.getReceptor().getLogin());
+            stmMensaxe.setTimestamp(3,m.getDataEnvio());
+            stmMensaxe.executeUpdate();
+            super.getConexion().commit();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                stmMensaxe.close();
+            } catch (SQLException e) {
+                System.out.println("Imposible pechar os cursores.");
+            }
+        }
     }
 
     protected ArrayList<Mensaxe> listarMensaxesRecibidos(String loginReceptor) throws SQLException{
         ArrayList<Mensaxe> mensaxes=new ArrayList<>();
         PreparedStatement stmMensaxe = null;
         ResultSet resultMensaxes;
-
-        stmMensaxe=super.getConexion().prepareStatement("SELECT * FROM enviarMensaxes WHERE receptor=? ORDER BY dataEnvio DESC;");
-        stmMensaxe.setString(1,loginReceptor);
-        resultMensaxes=stmMensaxe.executeQuery();
-        while (resultMensaxes.next()){
-            mensaxes.add(new Mensaxe(
-                    new Usuario(resultMensaxes.getString("emisor")),
-                    new Usuario(resultMensaxes.getString("receptor")),
-                    resultMensaxes.getTimestamp("dataEnvio"),
-                    resultMensaxes.getString("contido"),
-                    resultMensaxes.getBoolean("lido")
-            ));
+        try{
+            stmMensaxe=super.getConexion().prepareStatement("SELECT * FROM enviarMensaxes WHERE receptor=? ORDER BY dataEnvio DESC;");
+            stmMensaxe.setString(1,loginReceptor);
+            resultMensaxes=stmMensaxe.executeQuery();
+            while (resultMensaxes.next()){
+                mensaxes.add(new Mensaxe(
+                        new Usuario(resultMensaxes.getString("emisor")),
+                        new Usuario(resultMensaxes.getString("receptor")),
+                        resultMensaxes.getTimestamp("dataEnvio"),
+                        resultMensaxes.getString("contido"),
+                        resultMensaxes.getBoolean("lido")
+                ));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                stmMensaxe.close();
+            } catch (SQLException e) {
+                System.out.println("Imposible pechar os cursores.");
+            }
         }
         return mensaxes;
     }
