4ca33255ea35847bad9c63f76ca01b4072a363f8
David <davidcm62@gmail.com>
2020-04-02 20:36:22 +0200

    Commit

--- CentroDeportivo_Grupo3_1/src/centrodeportivo/gui/controladores/socios/vCuotaController.java
+++ CentroDeportivo_Grupo3_1/src/centrodeportivo/gui/controladores/socios/vCuotaController.java
@@ -1,6 +1,9 @@
 package centrodeportivo.gui.controladores.socios;
 
 import centrodeportivo.aplicacion.FachadaAplicacion;
+import centrodeportivo.aplicacion.obxectos.actividades.Actividade;
+import centrodeportivo.aplicacion.obxectos.area.Area;
+import centrodeportivo.aplicacion.obxectos.area.Instalacion;
 import centrodeportivo.aplicacion.obxectos.tarifas.Cuota;
 import centrodeportivo.aplicacion.obxectos.usuarios.Usuario;
 import centrodeportivo.gui.controladores.AbstractController;
@@ -8,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class vCuotaController extends AbstractController implements Initializable {
@@ -17,10 +21,10 @@ public class vCuotaController extends AbstractController implements Initializabl
     public TextField campoPrezoBase;
     public TextField campoPrezoExtras;
     public TextField campoMaxActividades;
-    public MenuButton campoActividades;
-    public MenuButton campoCursos;
+    public TreeView campoActividades;
+    public TreeView campoCursos;
     public Label campoPrezoTotal;
-    public TreeTableView campoPrezos;
+    public TreeView campoPrezos;
 
     private Usuario usuario;
 
@@ -34,9 +38,60 @@ public class vCuotaController extends AbstractController implements Initializabl
         Cuota cuota=super.getFachadaAplicacion().consultarCuota(this.usuario.getLogin());
         this.campoSocio.setText(cuota.getUsuario().getNome());
         this.campoTarifa.setText(cuota.getTarifa().getNome());
-        this.campoPrezoBase.setText(String.valueOf(cuota.getTarifa().getPrezoBase())+" €");
-        this.campoPrezoExtras.setText(String.valueOf(cuota.getTarifa().getPrezoExtras())+" €");
+        this.campoPrezoBase.setText(cuota.getTarifa().getPrezoBase() +" €");
+        this.campoPrezoExtras.setText(cuota.getTarifa().getPrezoExtras() +" €");
         this.campoMaxActividades.setText(String.valueOf(cuota.getTarifa().getMaxActividades()));
+        this.campoPrezoTotal.setText(cuota.getTotalPrezo()+" €");
 
+        //actividades
+        ArrayList<Actividade> a=new ArrayList<>();
+        Actividade ac=new Actividade(null,new Area(1,new Instalacion(1)));
+        ac.setNome("actividade pocha");
+        a.add(ac);
+        a.add(ac);
+        a.add(ac);
+        a.add(ac);
+        a.add(ac);
+        a.add(ac);
+        a.add(ac);
+        a.add(ac);
+
+        cuota.setActividadesMes(a);
+
+        /*
+
+            REVISAR A QUERY DE CONSULTAR CUOTA
+            AS ACTIVIDADES ESTAAS DEVOLVENDO MAL
+
+         */
+        /*
+            Esto non me convence
+         */
+
+        TreeItem<String> actividadesItem=new TreeItem<>("Actividades...");
+        for(Actividade act:cuota.getActividadesMes()){
+            actividadesItem.getChildren().add(new TreeItem<>(act.getNome()));
+        }
+        this.campoActividades.setRoot(actividadesItem);
+
+        //cursos
+
+
+
+        //tabla precios
+        TreeItem<String> actItem=new TreeItem<String>("Prezos Actividades");
+        actItem.getChildren().add(new TreeItem<String>("Total actividades realizadas: "+cuota.getActividadesMes().size()));
+        actItem.getChildren().add(new TreeItem<String>("Base actividades: "+cuota.getTarifa().getPrezoBase()));
+        actItem.getChildren().add(new TreeItem<String>("Extra actividades: "+cuota.getPrezoActividadesExtra()));
+        actItem.getChildren().add(new TreeItem<String>("Total actividades: "+cuota.getTotalActividades()));
+
+        TreeItem<String> curItem=new TreeItem<String>("Prezos Cursos");
+        curItem.getChildren().add(new TreeItem<String>("Total cursos realizados: "+cuota.getCursosMes().size()));
+        curItem.getChildren().add(new TreeItem<String>("Total cursos: "+cuota.getTotalCursos()));
+
+        TreeItem<String> root=new TreeItem<>("Desglose");
+        root.getChildren().add(actItem);
+        root.getChildren().add(curItem);
+        this.campoPrezos.setRoot(root);
     }
 }
