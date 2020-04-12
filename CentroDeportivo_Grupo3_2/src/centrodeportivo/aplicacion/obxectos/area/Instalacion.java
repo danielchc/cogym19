0177ea4392cc76ded506683345f88929e39a718a
ManuelBGomez <61062739+ManuelBGomez@users.noreply.github.com>
2020-04-12 18:48:42 +0200

    Commit

--- CentroDeportivo_Grupo3_2/src/centrodeportivo/aplicacion/obxectos/area/Instalacion.java
+++ CentroDeportivo_Grupo3_2/src/centrodeportivo/aplicacion/obxectos/area/Instalacion.java
@@ -1,59 +1,123 @@
 package centrodeportivo.aplicacion.obxectos.area;
 
+/**
+ * @author Manuel Bendaña
+ * @author Helena Castro
+ * @author Victor Barreiro
+ * Clase que almacenará información sobre as instalacións que se extraian da base de datos.
+ */
 public class Instalacion {
 
-    private int codInstalacion;
-    private String nome;
-    private String numTelefono;
-    private String direccion;
+    /**
+     * Atributos dunha instalación: coinciden coa información que se ten gardada na base de datos.
+     */
+    private int codInstalacion; //Código dunha instalación
+    private String nome; //Nome dunha instalación
+    private String numTelefono; //Número de telefono
+    private String direccion; //Dirección.
 
+    /**
+     * Constructor cun só argumento: o código da instalación (que é a clave primaria).
+     * @param codInstalacion
+     */
     public Instalacion(int codInstalacion) {
         this.codInstalacion = codInstalacion;
     }
 
+    /**
+     * Constructor con tres argumentos: usarase cando un usuario queira introducir unha nova instalación, dado que
+     * inicialmente non se coñecerá o seu código (ata que se inserte).
+     * @param nome
+     * @param numTelefono
+     * @param direccion
+     */
     public Instalacion(String nome, String numTelefono, String direccion) {
         this.nome = nome;
         this.numTelefono = numTelefono;
         this.direccion = direccion;
     }
 
+
+    /**
+     * Constructor con todos os argumentos, para os casos nos que se teña toda a información (ao recuperala).
+     * @param codInstalacion
+     * @param nome
+     * @param numTelefono
+     * @param direccion
+     */
     public Instalacion(int codInstalacion, String nome, String numTelefono, String direccion) {
         this(nome, numTelefono, direccion);
         this.codInstalacion = codInstalacion;
     }
 
+    /**
+     * Getter do código da instalación.
+     * @return codInstalación -> O código da instalación
+     */
     public int getCodInstalacion() {
         return codInstalacion;
     }
 
+    /**
+     * Setter do código da instalación
+     * @param codInstalacion
+     */
     public void setCodInstalacion(int codInstalacion) {
         this.codInstalacion = codInstalacion;
     }
 
+    /**
+     * Getter do nome da instalación
+     * @return nome -> Nome da instalación
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Setter do nome da instalación
+     * @param nome
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Getter do número de teléfono da instalación
+     * @return numTelefono -> O número de teléfono da instalación.
+     */
     public String getNumTelefono() {
         return numTelefono;
     }
 
+    /**
+     * Setter do número de teléfono da instalación
+     * @param numTelefono
+     */
     public void setNumTelefono(String numTelefono) {
         this.numTelefono = numTelefono;
     }
 
+    /**
+     * Getter da dirección da instalación:
+     * @return direccion -> A dirección da instalación
+     */
     public String getDireccion() {
         return direccion;
     }
 
+    /**
+     * Setter da dirección da instalación
+     * @param direccion
+     */
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
+    /**
+     * Método que permite convertir unha instalación a cadea de caracteres.
+     * @return instalacion representada en forma de caracteres polos seus campos.
+     */
     @Override
     public String toString() {
         return "Instalacion{" +
@@ -64,6 +128,11 @@ public class Instalacion {
                 '}';
     }
 
+    /**
+     * Método que comproba se dúas instalacións son iguais.
+     * @param obj
+     * @return booleano que indica se a instalación coincide coa pasada como argumento.
+     */
     @Override
     public boolean equals(Object obj){
         if(obj instanceof Instalacion){
