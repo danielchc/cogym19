59ae5cf4844369e6761811cb494f307125bf1fc6
VictorX <55930716+Vxbd@users.noreply.github.com>
2020-04-13 16:23:01 +0200

    Commit

--- CentroDeportivo_Grupo3_2/src/centrodeportivo/aplicacion/obxectos/area/Area.java
+++ CentroDeportivo_Grupo3_2/src/centrodeportivo/aplicacion/obxectos/area/Area.java
@@ -1,16 +1,27 @@
 package centrodeportivo.aplicacion.obxectos.area;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Objects;
+
+/*
+ * Autor: Víctor Barreiro
+ * Clase que xeral para almacenar información de Area.
+ * Incorporanse distintos construtres coa finalidade de que poida atender as distintas
+ * finalidades de xestión, incluso cando a informaicón recuperada da area non sexa completa.
+ * */
 
 public class Area {
-    private int codArea;
-    private Instalacion instalacion;
-    private String nome;
-    private String descricion;
     private int aforoMaximo;
+    private int codArea;
     private Date dataBaixa;
+    private String descricion;
+    private Instalacion instalacion;
+    private ArrayList<Material> materiais;
+    private String nome;
 
-    public Area(int codArea,Instalacion instalacion) {
+
+    public Area(int codArea, Instalacion instalacion) {
         this.codArea = codArea;
         this.instalacion = instalacion;
     }
@@ -22,13 +33,31 @@ public class Area {
         this.aforoMaximo = aforoMaximo;
     }
 
-    public Area(int codArea,Instalacion instalacion, String nome, String descricion, int aforoMaximo,Date dataBaixa) {
+    public Area(int codArea, Instalacion instalacion, String nome, String descricion, int aforoMaximo, Date dataBaixa) {
         this(instalacion, nome, descricion, aforoMaximo);
         this.codArea = codArea;
         this.dataBaixa = dataBaixa;
     }
 
-    /*clase de xoguete sei que non a tiña que facer eu pero faciame falta*/
+    public Area(int aforoMaximo, int codArea, Date dataBaixa, String descricion, Instalacion instalacion, ArrayList<Material> materiais, String nome) {
+        this.aforoMaximo = aforoMaximo;
+        this.codArea = codArea;
+        this.dataBaixa = dataBaixa;
+        this.descricion = descricion;
+        this.instalacion = instalacion;
+        this.materiais = materiais;
+        this.nome = nome;
+    }
+
+    //Getters e setters
+    public int getAforoMaximo() {
+        return aforoMaximo;
+    }
+
+    public void setAforoMaximo(int aforoMaximo) {
+        this.aforoMaximo = aforoMaximo;
+    }
+
 
     public int getCodArea() {
         return codArea;
@@ -38,21 +67,15 @@ public class Area {
         this.codArea = codArea;
     }
 
-    public Instalacion getInstalacion() {
-        return instalacion;
+
+    public Date getDataBaixa() {
+        return dataBaixa;
     }
 
-    public void setInstalacion(Instalacion instalacion) {
-        this.instalacion = instalacion;
+    public void setDataBaixa(Date dataBaixa) {
+        this.dataBaixa = dataBaixa;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
     public String getDescricion() {
         return descricion;
@@ -62,21 +85,33 @@ public class Area {
         this.descricion = descricion;
     }
 
-    public int getAforoMaximo() {
-        return aforoMaximo;
+
+    public Instalacion getInstalacion() {
+        return instalacion;
     }
 
-    public void setAforoMaximo(int aforoMaximo) {
-        this.aforoMaximo = aforoMaximo;
+    public void setInstalacion(Instalacion instalacion) {
+        this.instalacion = instalacion;
     }
 
-    public Date getDataBaixa() {
-        return dataBaixa;
+
+    public ArrayList<Material> getMatrais() {
+        return materiais;
     }
 
-    public void setDataBaixa(Date dataBaixa) {
-        this.dataBaixa = dataBaixa;
+    public void setMateriais(ArrayList<Material> materiais) {
+        this.materiais = materiais;
     }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 
     @Override
     public String toString() {
@@ -89,4 +124,21 @@ public class Area {
                 ", dataBaixa=" + dataBaixa +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Area area = (Area) o;
+        return codArea == area.codArea &&
+                Objects.equals(nome, area.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codArea, nome);
+    }
 }
+
+
