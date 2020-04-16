19740b9662e1b5d948a767c7fd70fe54ef831c02
ManuelBGomez <61062739+ManuelBGomez@users.noreply.github.com>
2020-04-16 22:05:17 +0200

    Commit

--- CentroDeportivo_Grupo3_2/src/centrodeportivo/aplicacion/obxectos/actividades/Curso.java
+++ CentroDeportivo_Grupo3_2/src/centrodeportivo/aplicacion/obxectos/actividades/Curso.java
@@ -1,14 +1,84 @@
 package centrodeportivo.aplicacion.obxectos.actividades;
 
-import centrodeportivo.aplicacion.obxectos.usuarios.Socio;
+import java.util.HashMap;
 
-import java.util.ArrayList;
 
+/**
+ * @author Manuel Bendaña
+ * @author Helena Castro
+ * @author Victor Barreiro
+ *
+ * Esta clase modela a información sobre os cursos almacenada na base de datos.
+ */
 public class Curso {
+    /**
+     * Atributos da clase:
+     */
     private int codCurso;
-    private String nombre;
+    private String nome;
     private String descricion;
-    private float precio;
-    private ArrayList<Socio> socios;
-    private ArrayList<Actividade> actividades;
+    private float duracion;
+    private float prezo;
+    private HashMap<String, Actividade> actividades;
+
+    //Constructores
+    public Curso(String nome, String descricion, float duracion){
+        this.nome = nome;
+        this.descricion = descricion;
+        this.duracion = duracion;
+    }
+
+    public Curso(int codCurso, String nome, String descricion, float prezo){
+        this(nome, descricion, prezo);
+        this.codCurso = codCurso;
+    }
+
+    //Getters e setters:
+    public void setCodCurso(int codCurso){
+        this.codCurso = codCurso;
+    }
+
+    public int getCodCurso(){
+        return codCurso;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setDescricion(String descricion){
+        this.descricion = descricion;
+    }
+
+    public String getDescricion(){
+        return descricion;
+    }
+
+    public void setDuracion(float duracion){
+        this.duracion = duracion;
+    }
+
+    public float getDuracion(){
+        return duracion;
+    }
+
+    public void setPrezo(float prezo){
+        this.prezo = prezo;
+    }
+
+    public float getPrezo(){
+        return prezo;
+    }
+
+    public void setActividades(HashMap<String, Actividade> actividades){
+        this.actividades = actividades;
+    }
+
+    public HashMap<String, Actividade> getActividades(){
+        return this.actividades;
+    }
 }
