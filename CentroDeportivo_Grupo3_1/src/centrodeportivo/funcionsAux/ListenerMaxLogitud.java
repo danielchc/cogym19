d4a2071cbf19e517402329b110486aabbb225b54
David <davidcm62@gmail.com>
2020-04-19 15:43:57 +0200

    Commit

--- CentroDeportivo_Grupo3_1/src/centrodeportivo/funcionsAux/ListenerMaxLogitud.java
+++ CentroDeportivo_Grupo3_1/src/centrodeportivo/funcionsAux/ListenerMaxLogitud.java
@@ -12,15 +12,29 @@ import javafx.scene.control.TextInputControl;
  */
 public final class ListenerMaxLogitud implements ChangeListener<String> {
 
+    /**
+     * Atributos
+     */
     private TextInputControl textField;
     private Integer maxLoxitude;
 
+    /**
+     * Constructor do listener
+     * @param textField campo de texto asociado
+     * @param maxLoxitude número de caractéres máximo
+     */
     public ListenerMaxLogitud(TextInputControl textField, Integer maxLoxitude) {
         this.textField = textField;
         this.maxLoxitude = maxLoxitude;
     }
 
 
+    /**
+     * Este método comprobar a lonxitude do texto do campo. Se se pasa, non permite escribir máis.
+     * @param observableValue
+     * @param valorAnterior Texto anterior a escribir.
+     * @param valorNovo Texto posterior.
+     */
     @Override
     public void changed(ObservableValue<? extends String> observableValue, String valorAnterior, String valorNovo) {
         if(valorAnterior==null || valorAnterior.equals("")) return;
