0dc5cda9930efef512fca89de24dec7e8606a167
David <davidcm62@gmail.com>
2020-04-20 10:12:15 +0200

    Commit

--- CentroDeportivo_Grupo3_1/src/centrodeportivo/gui/controladores/comun/vMensaxesController.java
+++ CentroDeportivo_Grupo3_1/src/centrodeportivo/gui/controladores/comun/vMensaxesController.java
@@ -48,33 +48,35 @@ public class vMensaxesController extends AbstractController implements Initializ
 
     /**
      * Constructor do controlador do buzón de mensaxes.
-     * @param fachadaAplicacion Fachada da aplicación.
+     *
+     * @param fachadaAplicacion    Fachada da aplicación.
      * @param vPrincipalController Controlador da vista principal.
      */
     public vMensaxesController(FachadaAplicacion fachadaAplicacion, vPrincipalController vPrincipalController) {
-        super(fachadaAplicacion,vPrincipalController);
-        this.fachadaAplicacion=super.getFachadaAplicacion();
+        super(fachadaAplicacion, vPrincipalController);
+        this.fachadaAplicacion = super.getFachadaAplicacion();
     }
 
     /**
      * Método para inicializar a vista dos mensaxes.
+     *
      * @param url
      * @param resourceBundle
      */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        this.receptor=super.getvPrincipalController().obterUsuarioLogeado();
+        this.receptor = super.getvPrincipalController().obterUsuarioLogeado();
 
         this.containerMensaxe.setVisible(false);
         this.containerChats.setPlaceholder(new Label("Buzón vacío"));
         TableColumn<Mensaxe, String> column = new TableColumn<>("Chats");
-        column.setCellValueFactory(c->new SimpleStringProperty(
-                "[Hora]:"+new Date(c.getValue().getDataEnvio().getTime()).toString()+"[Emisor]: "+c.getValue().getEmisor().getLogin()));
+        column.setCellValueFactory(c -> new SimpleStringProperty(
+                "[Hora]:" + new Date(c.getValue().getDataEnvio().getTime()).toString() + "[Emisor]: " + c.getValue().getEmisor().getLogin()));
         this.containerChats.setRowFactory(tv -> new TableRow<Mensaxe>() {
             @Override
             public void updateItem(Mensaxe item, boolean empty) {
-                super.updateItem(item, empty) ;
-                if ((item != null)&&(!item.isLido()))setStyle("-fx-font-weight: bold;");
+                super.updateItem(item, empty);
+                if ((item != null) && (!item.isLido())) setStyle("-fx-font-weight: bold;");
             }
         });
 
@@ -86,23 +88,24 @@ public class vMensaxesController extends AbstractController implements Initializ
 
     /**
      * Método para amosar a mensaxe seleccionada da lista de mensaxes recibidas.
+     *
      * @param mouseEvent evento
      */
     public void listenerTabla(MouseEvent mouseEvent) {
-        if(!this.containerChats.getSelectionModel().isEmpty()){
+        if (!this.containerChats.getSelectionModel().isEmpty()) {
             try {
-                Mensaxe mensaxe=(Mensaxe)this.containerChats.getSelectionModel().getSelectedItem();
-                this.labelEmisor.setText("Emisor: "+mensaxe.getEmisor().getLogin());
-                this.labelReceptor.setText("Receptor: "+mensaxe.getReceptor().getLogin());
-                Date data=new Date(mensaxe.getDataEnvio().getTime());
-                this.labelData.setText("Data: "+ data);
+                Mensaxe mensaxe = (Mensaxe) this.containerChats.getSelectionModel().getSelectedItem();
+                this.labelEmisor.setText("Emisor: " + mensaxe.getEmisor().getLogin());
+                this.labelReceptor.setText("Receptor: " + mensaxe.getReceptor().getLogin());
+                Date data = new Date(mensaxe.getDataEnvio().getTime());
+                this.labelData.setText("Data: " + data);
                 this.labelMensaxe.setText(mensaxe.getContido());
                 this.containerMensaxe.setVisible(true);
                 this.fachadaAplicacion.marcarMensaxeComoLido(mensaxe);
                 mensaxe.setLido(true);
                 this.containerChats.refresh();
             } catch (ExcepcionBD excepcionBD) {
-                super.getFachadaAplicacion().mostrarErro("Mensaxe",excepcionBD.getMessage());
+                super.getFachadaAplicacion().mostrarErro("Mensaxe", excepcionBD.getMessage());
             }
         }
     }
@@ -110,7 +113,7 @@ public class vMensaxesController extends AbstractController implements Initializ
     /**
      * Método para actualizar o buzón de mensaxes recibidas.
      */
-    public void actualizarTabla(){
+    public void actualizarTabla() {
         this.containerChats.getItems().removeAll(this.containerChats.getItems());
         this.containerChats.getItems().addAll(super.getFachadaAplicacion().listarMensaxesRecibidos(this.receptor.getLogin()));
     }
--- CentroDeportivo_Grupo3_1/src/centrodeportivo/gui/controladores/principal/vPrincipalController.java
+++ CentroDeportivo_Grupo3_1/src/centrodeportivo/gui/controladores/principal/vPrincipalController.java
@@ -79,23 +79,25 @@ public class vPrincipalController extends AbstractController implements Initiali
 
     /**
      * Constructor para o controlador da pantalla principal que contén o menú e o contenedor das distintas pantallas.
+     *
      * @param fachadaAplicacion fachada da aplicación
-     * @param usuarioLogeado Usuario que está activo
-     * @param pantallaAMostrar pantalla a mostrar en primeiro lugar
+     * @param usuarioLogeado    Usuario que está activo
+     * @param pantallaAMostrar  pantalla a mostrar en primeiro lugar
      */
-    public vPrincipalController(FachadaAplicacion fachadaAplicacion, Usuario usuarioLogeado,IdPantalla pantallaAMostrar) {
+    public vPrincipalController(FachadaAplicacion fachadaAplicacion, Usuario usuarioLogeado, IdPantalla pantallaAMostrar) {
         super(fachadaAplicacion);
         this.loginUsuarioLoggeado = usuarioLogeado.getLogin();
-        this.pantallaAMostrar=pantallaAMostrar;
-        this.transiciones=new HashMap<>();
-        this.botonesMenu=new ArrayList<>();
-        this.pantallas=new HashMap<>();
-        this.ultimaPantalla= IdPantalla.INICIO;
+        this.pantallaAMostrar = pantallaAMostrar;
+        this.transiciones = new HashMap<>();
+        this.botonesMenu = new ArrayList<>();
+        this.pantallas = new HashMap<>();
+        this.ultimaPantalla = IdPantalla.INICIO;
         cargarPantallas();
     }
 
     /**
      * Método para inicializar a vista.
+     *
      * @param url
      * @param resourceBundle
      */
@@ -112,52 +114,56 @@ public class vPrincipalController extends AbstractController implements Initiali
      */
     private void cargarPantallas() {
         //carganse todas as pantallas necesarias
-        this.pantallas.put(IdPantalla.NOVOUSUARIO,new DatosVista("../../vistas/persoal/usuarios/vNovoUsuario.fxml",new vNovoUsuarioController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.INICIO,new DatosVista("../../vistas/principal/vInicio.fxml",new vInicioController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.ADMINISTRARUSUARIOS,new DatosVista("../../vistas/persoal/usuarios/vAdministrarUsuarios.fxml",new vAdministrarUsuariosController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.NOVATARIFA,new DatosVista("../../vistas/persoal/tarifas/vNovaTarifa.fxml",new vNovaTarifaController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.ADMINISTRARTARIFAS,new DatosVista("../../vistas/persoal/tarifas/vAdministrarTarifas.fxml",new vAdministrarTarifasController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.MENSAXES,new DatosVista("../../vistas/comun/vMensaxes.fxml",new vMensaxesController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.NOVOMENSAXE,new DatosVista("../../vistas/comun/vNovoMensaxe.fxml",new vNovoMensaxeController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.CUOTA,new DatosVista("../../vistas/socios/vCuota.fxml",new vCuotaController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.RESUMENREXISTROS,new DatosVista("../../vistas/socios/vResumenRexistros.fxml",new vResumenRexistrosController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.NOVOREXISTRO,new DatosVista("../../vistas/socios/vNovoRexistro.fxml",new vNovoRexistroController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.ELIMINARREXISTRO,new DatosVista("../../vistas/socios/vEliminarRexistro.fxml",new vEliminarRexistroController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.ADMINISTRARINCIDENCIAS,new DatosVista("../../vistas/persoal/incidencias/vAdministrarIncidencias.fxml",new vAdministrarIncidenciasController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.ADMINISTRARCAPACIDADES,new DatosVista("../../vistas/persoal/usuarios/vAdministrarCapacidades.fxml",new vAdministrarCapacidadesController(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.NOVAINCIDENCIA,new DatosVista("../../vistas/comun/vNovaIncidencia.fxml",new vNovaIncidencia(super.getFachadaAplicacion(),this)));
-        this.pantallas.put(IdPantalla.XESTIONINCIDENCIA,new DatosVista("../../vistas/persoal/incidencias/vXestionIncidencia.fxml",new vXestionIncidenciaController(super.getFachadaAplicacion(),this)));
+        this.pantallas.put(IdPantalla.NOVOUSUARIO, new DatosVista("../../vistas/persoal/usuarios/vNovoUsuario.fxml", new vNovoUsuarioController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.INICIO, new DatosVista("../../vistas/principal/vInicio.fxml", new vInicioController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.ADMINISTRARUSUARIOS, new DatosVista("../../vistas/persoal/usuarios/vAdministrarUsuarios.fxml", new vAdministrarUsuariosController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.NOVATARIFA, new DatosVista("../../vistas/persoal/tarifas/vNovaTarifa.fxml", new vNovaTarifaController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.ADMINISTRARTARIFAS, new DatosVista("../../vistas/persoal/tarifas/vAdministrarTarifas.fxml", new vAdministrarTarifasController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.MENSAXES, new DatosVista("../../vistas/comun/vMensaxes.fxml", new vMensaxesController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.NOVOMENSAXE, new DatosVista("../../vistas/comun/vNovoMensaxe.fxml", new vNovoMensaxeController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.CUOTA, new DatosVista("../../vistas/socios/vCuota.fxml", new vCuotaController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.RESUMENREXISTROS, new DatosVista("../../vistas/socios/vResumenRexistros.fxml", new vResumenRexistrosController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.NOVOREXISTRO, new DatosVista("../../vistas/socios/vNovoRexistro.fxml", new vNovoRexistroController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.ELIMINARREXISTRO, new DatosVista("../../vistas/socios/vEliminarRexistro.fxml", new vEliminarRexistroController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.ADMINISTRARINCIDENCIAS, new DatosVista("../../vistas/persoal/incidencias/vAdministrarIncidencias.fxml", new vAdministrarIncidenciasController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.ADMINISTRARCAPACIDADES, new DatosVista("../../vistas/persoal/usuarios/vAdministrarCapacidades.fxml", new vAdministrarCapacidadesController(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.NOVAINCIDENCIA, new DatosVista("../../vistas/comun/vNovaIncidencia.fxml", new vNovaIncidencia(super.getFachadaAplicacion(), this)));
+        this.pantallas.put(IdPantalla.XESTIONINCIDENCIA, new DatosVista("../../vistas/persoal/incidencias/vXestionIncidencia.fxml", new vXestionIncidenciaController(super.getFachadaAplicacion(), this)));
     }
 
     /**
      * Este método inicializa as transicións dos sliders desplegables.
      * Ademáis esconde todos os desplegables ao iniciarse a aplicación.
      */
-    private void inciarTransiciones(){
-        ArrayList<VBox> sliders=new ArrayList<>();
-        sliders.add(sideBarUsuariosP);       sliders.add(sideBarMensaxesP);
+    private void inciarTransiciones() {
+        ArrayList<VBox> sliders = new ArrayList<>();
+        sliders.add(sideBarUsuariosP);
+        sliders.add(sideBarMensaxesP);
         sliders.add(sideBarTarifasP);
-            sliders.add(sideBarIncidenciasP);
+        sliders.add(sideBarIncidenciasP);
 
-        sliders.add(sideBarUsuariosS);       sliders.add(sideBarMensaxesS);
-            sliders.add(sideBarIncidenciasS);
+        sliders.add(sideBarUsuariosS);
+        sliders.add(sideBarMensaxesS);
+        sliders.add(sideBarIncidenciasS);
 
-        botonesMenu.add(btnUsuariosP);       botonesMenu.add(btnMensaxesP);
+        botonesMenu.add(btnUsuariosP);
+        botonesMenu.add(btnMensaxesP);
         botonesMenu.add(btnTarifasP);
-            botonesMenu.add(btnIncidenciaP);
+        botonesMenu.add(btnIncidenciaP);
 
-        botonesMenu.add(btnUsuariosS);       botonesMenu.add(btnMensaxesS);
-            botonesMenu.add(btnIncidenciaS);
+        botonesMenu.add(btnUsuariosS);
+        botonesMenu.add(btnMensaxesS);
+        botonesMenu.add(btnIncidenciaS);
 
-        for(int i=0;i<botonesMenu.size();i++){
-            this.transiciones.put(botonesMenu.get(i),new Transicion(sliders.get(i)));
+        for (int i = 0; i < botonesMenu.size(); i++) {
+            this.transiciones.put(botonesMenu.get(i), new Transicion(sliders.get(i)));
         }
 
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
-                for(VBox v:sliders){
-                    TranslateTransition t=new TranslateTransition(Duration.millis(.1),v);
+                for (VBox v : sliders) {
+                    TranslateTransition t = new TranslateTransition(Duration.millis(.1), v);
                     t.setToX(-(v.getWidth()));
                     t.play();
                 }
@@ -168,14 +174,14 @@ public class vPrincipalController extends AbstractController implements Initiali
     /**
      * Este método mostra os menús correspondentes segundo o tipo de usuario que se loggea.
      */
-    private void ocultarMenusInnecesarios(){
-        if(pantallaAMostrar==IdPantalla.PANTALLAPERSOAL){
+    private void ocultarMenusInnecesarios() {
+        if (pantallaAMostrar == IdPantalla.PANTALLAPERSOAL) {
             this.menuSocio.setVisible(false);
             sideBarUsuariosS.setVisible(false);
             sideBarMensaxesS.setVisible(false);
             sideBarIncidenciasS.setVisible(false);
 
-        }else if(pantallaAMostrar==IdPantalla.PANTALLASOCIO){
+        } else if (pantallaAMostrar == IdPantalla.PANTALLASOCIO) {
             this.menuPersoal.setVisible(false);
             sideBarUsuariosP.setVisible(false);
             sideBarMensaxesP.setVisible(false);
@@ -186,12 +192,13 @@ public class vPrincipalController extends AbstractController implements Initiali
 
     /**
      * Este método esconde o desplegable correspondente ao botón pulsado.
+     *
      * @param boton Botón do desplegable pulsado.
      */
-    private void esconderSliderBotonMenu(Button boton){
+    private void esconderSliderBotonMenu(Button boton) {
         boton.getStyleClass().remove("sidebar-button-active");
         boton.getStyleClass().add("sidebar-button");
-        Transicion t=this.transiciones.get(boton);
+        Transicion t = this.transiciones.get(boton);
         t.getTransicionCerrar().setToX(-(t.getSlider().getWidth()));
         t.getTransicionCerrar().play();
     }
@@ -199,21 +206,22 @@ public class vPrincipalController extends AbstractController implements Initiali
     /**
      * Este método esconde todos os desplegables.
      */
-    private void esconderTodosSliders(){
-        for(Button b:this.botonesMenu){
+    private void esconderTodosSliders() {
+        for (Button b : this.botonesMenu) {
             esconderSliderBotonMenu(b);
         }
     }
 
     /**
      * Este método abre/cerra o desplegable asociado ao botón do menú pulsado.
+     *
      * @param actionEvent evento
      */
     public void btnMenuAction(ActionEvent actionEvent) {
-        Button boton=(Button)actionEvent.getSource();
-        Transicion t=this.transiciones.get(boton);
+        Button boton = (Button) actionEvent.getSource();
+        Transicion t = this.transiciones.get(boton);
         esconderTodosSliders();
-        if ((t.getSlider().getTranslateX()) == -(t.getSlider().getWidth()) ) {
+        if ((t.getSlider().getTranslateX()) == -(t.getSlider().getWidth())) {
             boton.getStyleClass().remove("sidebar-button");
             boton.getStyleClass().add("sidebar-button-active");
             t.getTransicionAbrir().play();
@@ -224,28 +232,30 @@ public class vPrincipalController extends AbstractController implements Initiali
 
     /**
      * Este método esconde os desplegables e abre a pantalla seleccionada.
+     *
      * @param actionEvent evento
      */
     public void btnSliderAction(ActionEvent actionEvent) {
         esconderTodosSliders();
-        mostrarMenu(IdPantalla.valueOf(((Button)actionEvent.getSource()).getId()));
+        mostrarMenu(IdPantalla.valueOf(((Button) actionEvent.getSource()).getId()));
     }
 
     /**
      * Este método mostra a pantalla correspondente ao id que se lle pasa como parámetro.
      * Cárgase o fxml e asóciaselle o seu controlador correspondente.
+     *
      * @param idPantalla Pantalla a mostrar.
      */
-    public void mostrarMenu(IdPantalla idPantalla){
-        this.ultimaPantalla=this.pantallaAMostrar;
+    public void mostrarMenu(IdPantalla idPantalla) {
+        this.ultimaPantalla = this.pantallaAMostrar;
         this.mainContainer.getChildren().removeAll(this.mainContainer.getChildren());
         try {
-            DatosVista dv=this.pantallas.get(idPantalla);
-            FXMLLoader fxmlLoader=new FXMLLoader();
+            DatosVista dv = this.pantallas.get(idPantalla);
+            FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setController(dv.getControlador());
             fxmlLoader.setLocation(getClass().getResource(dv.getPathFXML()));
             this.mainContainer.getChildren().add(fxmlLoader.load());
-            this.pantallaAMostrar=idPantalla;
+            this.pantallaAMostrar = idPantalla;
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -254,32 +264,33 @@ public class vPrincipalController extends AbstractController implements Initiali
     /**
      * Este método permite volver á última pantalla amosada.
      */
-    public void volverAtras(){
+    public void volverAtras() {
         mostrarMenu(ultimaPantalla);
     }
 
     /**
      * Este método abre o perfil dun socio para poder editalo.
      */
-    public void perfilUsuarioAction(){
+    public void perfilUsuarioAction() {
         esconderTodosSliders();
         mostrarMenu(IdPantalla.NOVOUSUARIO);
-        ((vNovoUsuarioController)getControlador(IdPantalla.NOVOUSUARIO)).cargarDatosUsuario(super.getFachadaAplicacion().consultarUsuario(this.loginUsuarioLoggeado));
+        ((vNovoUsuarioController) getControlador(IdPantalla.NOVOUSUARIO)).cargarDatosUsuario(super.getFachadaAplicacion().consultarUsuario(this.loginUsuarioLoggeado));
     }
 
     /**
      * Este método devolve o controlador dunha pantalla.
+     *
      * @param idPantalla Identificador da pantalla.
      * @return Controladr asociado á pantalla.
      */
-    public AbstractController getControlador(IdPantalla idPantalla){
+    public AbstractController getControlador(IdPantalla idPantalla) {
         return this.pantallas.get(idPantalla).getControlador();
     }
 
     /**
      * @return Usuario loggeado no sistema.
      */
-    public Usuario obterUsuarioLogeado(){
+    public Usuario obterUsuarioLogeado() {
         return super.getFachadaAplicacion().consultarUsuario(loginUsuarioLoggeado);
     }
 }
