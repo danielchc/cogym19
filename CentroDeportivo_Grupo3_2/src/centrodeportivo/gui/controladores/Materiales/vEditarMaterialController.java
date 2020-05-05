0a9efbdc97aa50d92e44665b5cbd88e83c90bd3c
helenacastr <helenacastro0511@gmail.com>
2020-05-05 20:45:23 +0200

    Commit

--- CentroDeportivo_Grupo3_2/src/centrodeportivo/gui/controladores/Materiales/vEditarMaterialController.java
+++ CentroDeportivo_Grupo3_2/src/centrodeportivo/gui/controladores/Materiales/vEditarMaterialController.java
@@ -1,15 +1,33 @@
 package centrodeportivo.gui.controladores.Materiales;
 
 import centrodeportivo.aplicacion.FachadaAplicacion;
+import centrodeportivo.aplicacion.obxectos.area.Area;
+import centrodeportivo.aplicacion.obxectos.area.Instalacion;
+import centrodeportivo.aplicacion.obxectos.area.TipoMaterial;
 import centrodeportivo.gui.controladores.AbstractController;
 import centrodeportivo.gui.controladores.principal.vPrincipalController;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.util.StringConverter;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class vEditarMaterialController extends AbstractController implements Initializable {
 
     // Atributos públicos: referentes a ventá correspondente
+    public TextField campoCodigo;
+    public ComboBox<TipoMaterial> comboTipoMaterial = new ComboBox<>();
+    public TextField campoEstadoMaterial;
+    public ComboBox<Instalacion> comboInstalacion = new ComboBox<>();
+    public DatePicker campoDataCompraMaterial;
+    public ComboBox<Area> comboArea = new ComboBox<>();
+    public TextField campoPrezoMaterial;
+    public Button btnGardarMaterial;
+    public Button btnLimparMaterial;
 
     // Atributos privados: matenmos o controlador da ventá de procedencia
     private vPrincipalController controllerPrincipal;
@@ -24,6 +42,61 @@ public class vEditarMaterialController extends AbstractController implements Ini
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        // Inicializamos o comboBox dos tipos de materiais:
+        comboTipoMaterial.setItems(getFachadaAplicacion().listarTiposMateriais());
+        comboTipoMaterial.setConverter(new StringConverter<TipoMaterial>() {
+            @Override
+            public String toString(TipoMaterial object) {
+                return object.getNome();
+            }
+
+            @Override
+            public TipoMaterial fromString(String string) {
+                return comboTipoMaterial.getItems().stream().filter(ap ->
+                        ap.getNome().equals(string)).findFirst().orElse(null);
+            }
+        });
+
+        // Inicializamos o comboBox das areas dunha instalacion
+        comboArea.setConverter(new StringConverter<Area>() {
+            @Override
+            public String toString(Area object) {
+                return object.getNome();
+            }
+
+            @Override
+            public Area fromString(String string) {
+                return comboArea.getItems().stream().filter(ap ->
+                        ap.getNome().equals(string)).findFirst().orElse(null);
+            }
+        });
+
+        // Inicializamos o comboBox das instalacions
+        comboInstalacion.setItems(getFachadaAplicacion().listarInstalacions());
+
+        comboInstalacion.setConverter(new StringConverter<Instalacion>() {
+            @Override
+            public String toString(Instalacion object) {
+                return object.getNome();
+            }
+
+            @Override
+            public Instalacion fromString(String string) {
+                return comboInstalacion.getItems().stream().filter(ap ->
+                        ap.getNome().equals(string)).findFirst().orElse(null);
+            }
+        });
+
+        comboInstalacion.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == null) {
+                comboArea.getItems().clear();
+                comboArea.setDisable(true);
+            } else {
+                comboArea.getItems().setAll(getFachadaAplicacion().listarAreasInstalacion(newValue));
+                comboArea.setDisable(false);
+                comboArea.setValue(null);
+            }
+        });
     }
 
 }
